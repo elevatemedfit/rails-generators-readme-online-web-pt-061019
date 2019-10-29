6b2e4701cd416495fd87109aba0406ee57709b54
Chris Junker <elevatemedfit@gmail.com>
2019-10-29 14:54:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029144251) do
+ActiveRecord::Schema.define(version: 20191029145039) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20191029144251) do
     t.datetime "updated_at",       null: false
     t.integer  "category_id"
     t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
